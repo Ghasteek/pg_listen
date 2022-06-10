435204f624150d342ccccf69af98e9e1c471eef8
Tomáš Podolský <podolsky.tomas@gmail.com>
2022-06-10 15:01:39 +0200

    fix: better logging and default config file

--- pg_listen.js
+++ pg_listen.js
@@ -6,13 +6,18 @@ const fs = require('fs');
 const argv = require('yargs')
 	.option('config', {
 		alias: 'c',
-		describe: 'path to file/directory with configuration',
+		describe: 'path to file with configuration',
 	})
-	.demandOption('config', 'Please provide path to file/directory with configuration')
 	.help()
 	.argv;
 
-var config = yaml.load(fs.readFileSync(argv.config, 'utf8'));
+var config = yaml.load(fs.readFileSync(argv.config || './config.cfg', 'utf8'));
+
+// is there configuration to DB?
+if (!config || !config.pgasync_url) {
+    console.log('\nConfig file is empty, or does not contains mandatory informations.\n');
+    process.exit(1);
+}
 let db = parse(config.pgasync_url);
 
 const client = new Client({
@@ -22,19 +27,36 @@ const client = new Client({
   database: db.database,
   port: db.port,
 })
-client.connect()
 
-config.listen.forEach((channel) => {
-    client.query(`LISTEN ${channel}`)
-    console.log('Listening to ', channel)
+client.connect().catch((err) => {
+    console.error('\nCannot connect to DB.\n', err);
+    process.exit(1);
 });
 
+// test query
+client.query('SELECT 1 as test')
+    .then((resp) => {
+        console.log(`\nConnected to database "${db.database}" at ${db.host}:${db.port}.\n`);
+
+        // signing listeners for notifications
+        config.listen.forEach((channel) => {
+            client.query(`LISTEN ${channel}`)
+            console.log(`Listening to notifications "${channel}".`);
+        });
+        console.log('\n');
+    })
+    .catch((err) => {
+        console.error('\nTest query was rejected.', err, '\n');
+        process.exit(1);
+    });
+
+// Console log incoming notifications
 client.on('notification', msg => {
-    console.log('\n\n', msg.channel, ' - ');
+    console.log('\nIncoming notification "', msg.channel, '" -');
     try {
         console.log(JSON.stringify(JSON.parse(msg.payload), null, "  "));
     } catch {
         console.log(msg.payload);
     }
-    console.log('\n\n')
+    console.log('\n')
 })
